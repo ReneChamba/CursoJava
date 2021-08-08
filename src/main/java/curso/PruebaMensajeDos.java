5cb9082945420fb3a4982d0a7b1be310ba32404c
Ronny <ronnychamba96@gmail.com>
2021-08-08 14:43:36 -0500

    Add archivo sms 2

---
+++ src/main/java/curso/PruebaMensajeDos.java
@@ -0,0 +1,10 @@
+package curso;
+
+public class PruebaMensajeDos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola esto es un mensaje numero dos");
+	}
+
+}
